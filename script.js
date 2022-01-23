c699274c5a279036d76c268e338bfa4eb73f7752
allan <allan.riberio@gmail.com>
2022-01-23 20:26:16 -0300

    atualizado

---
+++ script.js
@@ -0,0 +1,26 @@
+console.log("1")
+
+for(let i = 1; i <= 10; i++){
+    console.log(i)
+}
+console.log("2")
+
+let contador = 10
+
+while(contador >= 1){
+    console.log(contador)
+    contador--
+}
+console.log("3")
+
+let cont = 1
+
+while (cont <= 100){
+    console.log(cont)
+    cont +=2
+}
+console.log("4")
+
+for(i = 0; i <= 100; i += 2){
+    console.log(i)
+}
